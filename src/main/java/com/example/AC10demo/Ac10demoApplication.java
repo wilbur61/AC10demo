9bb2cb68b8814c25d1b83d69ad7e2810763939ac
wilbur61 <wroales@perscholas.org>
2022-07-20 15:28:24 -0400

    Added my project

---
+++ src/main/java/com/example/AC10demo/Ac10demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AC10demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac10demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac10demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AC10demo/Ac10demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AC10demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac10demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
