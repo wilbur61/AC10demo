df2d7b4a1be04780de721cedac68a4a425dc4627
wilbur61 <wroales@perscholas.org>
2024-03-01 19:17:24 -0500

    updated to Spring 3.2.2  jakarta Eliminated duplicate call to updateTutoral in Controller

--- src/main/java/com/example/AC10demo/TutorialController.java
+++ src/main/java/com/example/AC10demo/TutorialController.java
@@ -47,17 +47,20 @@ public class TutorialController {
 // To update a tutorial record, we used the same save() and findById()
    @PutMapping("/tutorials/{id}")
    public void updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-       Optional<Tutorial> tutorialData = tutorialServices.getTutorialById(id);
        System.out.println("IN PutMapping(/tutorials/{id}");
-       if (tutorialData.isPresent()) {
-           tutorialServices.updateTutorial(id,tutorial);
-           // Eliminating duplicate code below
+       tutorialServices.updateTutorial(id,tutorial);
+
+       //  Below code is Not required
+       //Optional<Tutorial> tutorialData = tutorialServices.getTutorialById(id);
+       //
+       //if (tutorialData.isPresent()) {
+       //    tutorialServices.updateTutorial(id,tutorial);
            //Tutorial _tutorial = tutorialData.get();
            //_tutorial.setTitle(tutorial.getTitle());
            //_tutorial.setDescription(tutorial.getDescription());
            //_tutorial.setPublished(tutorial.isPublished());
           // tutorialServices.addTutorial(_tutorial);
-       }
+       //}
    }
 
    @DeleteMapping("/tutorials/{id}")
